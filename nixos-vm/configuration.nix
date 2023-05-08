ca1ed3a2ca9ce2c3feccf1e7aaf15d23155e6043
Bryan Hyshka <bryan@hyshka.com>
2023-05-08 17:36:16 -0600

    Enable zsh shell

--- nixos-vm/configuration.nix
+++ nixos-vm/configuration.nix
@@ -83,7 +83,6 @@
   # Allow users to use zsh shell
   programs.zsh.enable = true;
   environment.shells = with pkgs; [ zsh ];
-  users.defaultUserShell = pkgs.zsh;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
@@ -93,6 +92,7 @@
 	"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCTi8LybJv9rM1PY+89RizysnNS0qe17peP1lsribcY+VuEW1aZrjYePJKVyFlIUqQnPGr9zK2FsLqU+Y40hfNQMITlHQMCbrWLvGdPvR2uP17+DFvZSp+ox0KVIjqOgxpLIWbszHKzfA1g8FJfzpH7j1kzP7bEonUXAGd3eVtf2kuzELKl7pI4uQyuoKF6ti1EMKQEOivLJm9aphz8/Bk+aZVgFy2srCxhqpWM5v967iNOK+UtPAqStrkJTvc1NtmMe6YQ099lRltq5dLerBfb0r5BdTKa+oTrgMELzV1YOv1i5Nj21RUz0kDT1eiVoqmyYAIlB8Rn01qByCU+2FH1 bryan@hyshka.com"
       ];
       extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      shell = pkgs.zsh;
     };
   };
 
