25d62e61c9f0f74362c8ab76fc7617842db4848e
Bryan Hyshka <bryan@hyshka.com>
2023-05-08 17:07:18 -0600

    Add some shell configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,27 +42,122 @@
   home.packages = with pkgs; [
     # utils
     ncdu ranger htop
+
+    # fonts
+    iosevka-bin
   ];
 
-  programs.neovim.enable = true;
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+  };
+
+  #programs.kitty = {
+  #  enable = true;
+  #  font = {
+  #    size = 11;
+  #  };
+  #};
 
   programs.tmux = {
     enable = true;
+    tmuxp.enable = true;
     keyMode = "vi";
+    shortcut = "a";
+    terminal = "tmux-256color";
     customPaneNavigationAndResize = true;
     shortcut = "Space";
     baseIndex = 1;
     aggressiveResize = true;
     escapeTime = 10;
+    disableConfirmationPrompt = true;
+    reverseSplit = true;
     plugins = with pkgs; [ tmuxPlugins.sensible tmuxPlugins.pain-control tmuxPlugins.sessionist tmuxPlugins.tmux-colors-solarized ];
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.sessionist
+      tmuxPlugins.pain-control
+      tmuxPlugins.prefix-highlight
+      tmuxPlugins.fzf-tmux-url
+      tmuxPlugins.copycat
+      tmuxPlugins.yank
+    ];
     extraConfig = ''
-      set -g @colors-solarized 'light'
+      # Switch between last used window
+      bind-key C-a last-window
+      # Synchronize mode, send same command to all panes
+      # I hate the default map
+      bind a set-window-option synchronize-panes
+      # Make copy mode like visual mode in vim
+      bind Escape copy-mode
+      bind-key -T copy-mode-vi 'v' send -X begin-selection
+      bind-key -T copy-mode-vi 'Space' send -X halfpage-down
+      bind-key -T copy-mode-vi 'BSpace' send -X halfpage-up
+      # 24 bit color
+      set-option -sa terminal-features ',xterm-kitty:RGB'
+      # statusline
+      set -g status-left '#{prefix_highlight}'
+      set -g status-right '#{?pane_synchronized, #[bg=blue]#[fg=white] SYNCHRONIZED #[default],} #S '
+      # window status
+      setw -g window-status-format " #F#I:#W#F "
+      setw -g window-status-current-format " #F#I:#W#F "
     '';
   };
 
-  # Enable home-manager and git
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    defaultKeymap = "viins";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    history = {
+      ignoreDups = true;
+      expireDuplicatesFirst = true;
+    };
+    sessionVariables = {
+      # zsh-users config
+      ZSH_AUTOSUGGEST_USE_ASYNC = 1;
+      ZSH_HIGHLIGHT_HIGHLIGHTERS = [ "main" "brackets" "cursor" ];
+    };
+    # TODO ZIMFW
+    #initExtraBeforeCompInit = ''
+    #  # zimfw config
+    #  zstyle ':zim:input' double-dot-expand yes
+    #'';
+    plugins = [
+      {
+        name = "zim-completion";
+        src = pkgs.fetchFromGitHub {
+	  owner = "zimfw";
+	  repo = "completion";
+	};
+        file = "init.zsh";
+      }
+    #  {
+    #    src = zsh-completions;
+    #    name = "zsh-completions";
+    #  }
+    #  {
+    #    src = zsh-syntax-highlighting;
+    #    name = "zsh-syntax-highlighting";
+    #  }
+    #  # Should be the last one
+    #  {
+    #    src = zsh-history-substring-search;
+    #    name = "zsh-history-substring-search";
+    #  }
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "hyshka";
+    userEmail = "bryan@hyshka.com";
+  };
+
+  # Enable home-manager
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
