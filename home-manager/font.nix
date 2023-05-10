c2282ccd4a86c9dfd0b4db7d341fa540ba68b876
Bryan Hyshka <bryan@hyshka.com>
2023-05-10 06:11:35 -0600

    Correct font names

--- home-manager/font.nix
+++ home-manager/font.nix
@@ -2,15 +2,15 @@
   fontProfiles = {
     enable = true;
     monospace = {
-      family = "Iosevka";
+      family = "Iosevka Comfy";
       package = pkgs.iosevka-comfy.comfy;
     };
     sans-serif = {
-      family = "Iosevka";
+      family = "Iosevka Comfy Duo";
       package = pkgs.iosevka-comfy.comfy-duo;
     };
     serif = {
-      family = "Iosevka";
+      family = "Iosevka Comfy Motion Duo";
       package = pkgs.iosevka-comfy.comfy-motion-duo;
     };
   };
