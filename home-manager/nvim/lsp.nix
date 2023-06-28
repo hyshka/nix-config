be24c36f7e1f5c4233b325173c4526be983f55bd
hyshka <bryan@hyshka.com>
2023-06-28 09:08:35 -0600

    Disable trouble.nvim icons

--- home-manager/nvim/lsp.nix
+++ home-manager/nvim/lsp.nix
@@ -26,6 +26,23 @@
         plugin = trouble-nvim;
         type = "lua";
         config = /* lua */ ''
+          local trouble = require('trouble')
+	  -- settings without a patched font or icons
+          trouble.setup({
+	    icons = false,
+	    fold_open = "v", -- icon used for open folds
+	    fold_closed = ">", -- icon used for closed folds
+	    indent_lines = false, -- add an indent guide below the fold icons
+	    signs = {
+	      -- icons / text used for a diagnostic
+	      error = "error",
+	      warning = "warn",
+	      hint = "hint",
+	      information = "info"
+	    },
+	    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+	  })
+
 	  vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
             {silent = true, noremap = true}
           )
