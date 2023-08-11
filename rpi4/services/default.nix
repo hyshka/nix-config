1b93efc4f7e2d7b264d56af1ec61982b86a69402
hyshka <bryan@hyshka.com>
2023-08-11 08:20:11 -0600

    Test snapper config

--- rpi4/services/default.nix
+++ rpi4/services/default.nix
@@ -3,16 +3,17 @@
   services.snapper = {
       configs = {
               storage = {
-                  subvolume = "/mnt/storage";
-                  extraConfig = ''
-                    TIMELINE_CREATE=yes
-                    TIMELINE_CLEANUP=yes
-                      TIMELINE_LIMIT_HOURLY=12
-                      TIMELINE_LIMIT_DAILY=7
-                      TIMELINE_LIMIT_WEEKLY=4
-                      TIMELINE_LIMIT_MONTHLY=3
-                      TIMELINE_LIMIT_YEARLY=0
-                  '';
+                  SUBVOLUME = "/mnt/storage";
+		  TIMELINE_CREATE = true;
+		  TIMELINE_CLEANUP = true;
+                  TIMELINE_LIMIT_YEARLY=0;
+                  #extraConfig = ''
+                  #    TIMELINE_LIMIT_HOURLY=12
+                  #    TIMELINE_LIMIT_DAILY=7
+                  #    TIMELINE_LIMIT_WEEKLY=4
+                  #    TIMELINE_LIMIT_MONTHLY=3
+                  #    TIMELINE_LIMIT_YEARLY=0
+                  #'';
                 };
       };
   };
