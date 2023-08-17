1ebe317c8cb0220325e19e13bb3c16b7f61764f3
hyshka <bryan@hyshka.com>
2023-08-17 08:39:14 -0600

    Move rpi4 password to secrets

--- rpi4/configuration.nix
+++ rpi4/configuration.nix
@@ -78,8 +78,7 @@
     users = {
       hyshka = {
         isNormalUser = true;
-        initialPassword = "pw123";
-	# TODO passwordFile
+        passwordFile = config.sops.secrets.hyshka_password.path;
         extraGroups = [ "wheel" ];
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCTi8LybJv9rM1PY+89RizysnNS0qe17peP1lsribcY+VuEW1aZrjYePJKVyFlIUqQnPGr9zK2FsLqU+Y40hfNQMITlHQMCbrWLvGdPvR2uP17+DFvZSp+ox0KVIjqOgxpLIWbszHKzfA1g8FJfzpH7j1kzP7bEonUXAGd3eVtf2kuzELKl7pI4uQyuoKF6ti1EMKQEOivLJm9aphz8/Bk+aZVgFy2srCxhqpWM5v967iNOK+UtPAqStrkJTvc1NtmMe6YQ099lRltq5dLerBfb0r5BdTKa+oTrgMELzV1YOv1i5Nj21RUz0kDT1eiVoqmyYAIlB8Rn01qByCU+2FH1 bryan@hyshka.com"
@@ -149,6 +148,11 @@
     extraConfig = ''
       set -g @colors-solarized 'light'
     '';
+  };
+
+  sops.secrets.hyshka_password = {
+    sopsFile = ./secrets.yaml;
+    neededForUsers = true;
   };
 
   system.stateVersion = "23.05";
