069d214225064da8be8f7d100accd9df695590e4
hyshka <bryan@hyshka.com>
2023-08-17 08:51:14 -0600

    Fix missing var

--- rpi4/configuration.nix
+++ rpi4/configuration.nix
@@ -1,7 +1,7 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, outputs, pkgs, ... }: {
+{ inputs, outputs, pkgs, config, ... }: {
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
