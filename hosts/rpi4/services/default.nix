cb0526e949efefb5e48b28e46ccd95450a1744bc
Bryan Hyshka <bryan@hyshka.com>
2023-08-28 22:23:52 +0000

    rpi4: disable snapper service

--- hosts/rpi4/services/default.nix
+++ hosts/rpi4/services/default.nix
@@ -58,21 +58,21 @@
     # TODO healthcheck notification for sync & scrub
   };
 
-  services.snapper = {
-      configs = {
-              storage = {
-	          # TODO should this go through mergerfs?
-                  SUBVOLUME = "/mnt/disk1/hyshka";
-		  TIMELINE_CREATE = true;
-		  TIMELINE_CLEANUP = true;
-                  TIMELINE_LIMIT_HOURLY = 12;
-                  TIMELINE_LIMIT_DAILY = 7;
-                  TIMELINE_LIMIT_WEEKLY = 4;
-                  TIMELINE_LIMIT_MONTHLY = 3;
-                  TIMELINE_LIMIT_YEARLY = 0;
-                };
-      };
-  };
+  #services.snapper = {
+  #    configs = {
+  #            storage = {
+  #                # TODO should this go through mergerfs?
+  #                SUBVOLUME = "/mnt/disk1/hyshka";
+  #      	  TIMELINE_CREATE = true;
+  #      	  TIMELINE_CLEANUP = true;
+  #                TIMELINE_LIMIT_HOURLY = 12;
+  #                TIMELINE_LIMIT_DAILY = 7;
+  #                TIMELINE_LIMIT_WEEKLY = 4;
+  #                TIMELINE_LIMIT_MONTHLY = 3;
+  #                TIMELINE_LIMIT_YEARLY = 0;
+  #              };
+  #    };
+  #};
 
   services.restic.backups.hyshka = {
     user = "hyshka";
