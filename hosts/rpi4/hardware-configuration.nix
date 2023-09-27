5d65d0c58aa240ac9c9572d4f610896dbb81e3c1
hyshka <bryan@hyshka.com>
2023-09-27 09:40:10 -0600

    rpi4: remove disks

--- hosts/rpi4/hardware-configuration.nix
+++ hosts/rpi4/hardware-configuration.nix
@@ -37,39 +37,39 @@
       fsType = "ext4";
       options = [ "defaults" "noatime" ];
     };
-    "/mnt/disk1" = {
-      # newer drive, no errors: /dev/disk/by-id/usb-WD_Elements_2621_57584A3241363146314A4858-0:0-part1
-      device = "/dev/disk/by-id/usb-WD_Elements_2621_57584A3241363146314A4858-0:0-part1";
-      # older drive, some read errors: /dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1
-      fsType = "btrfs";
-      options = [ "defaults" "noatime" ];
-    };
-    "/mnt/disk2" = {
-      # older drive, some read errors: /dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1
-      device = "/dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1";
-      fsType = "btrfs";
-      options = [ "defaults" "noatime" ];
-    };
-    "/mnt/storage" = {
-      device = "/mnt/disk*";
-      fsType = "fuse.mergerfs";
-      options = [
-        "defaults"
-	# partial cache required for mmap support for qbittorrent
-	# ref: https://github.com/trapexit/mergerfs#you-need-mmap-used-by-rtorrent-and-many-sqlite3-base-software
-        "cache.files=partial"
-        "dropcacheonclose=true"
-        "category.create=mfs"
-        "fsname=mergerfs"
-        # TODO enable once I actually have free space
-        #"minfreespace=200G"
-      ];
-    };
-    "/mnt/parity1" = {
-      device = "/dev/disk/by-id/usb-WDC_WD40_EFPX-68C6CN0_152D00539000-0:0-part1";
-      fsType = "ext4";
-      options = [ "defaults" "noatime" ];
-    };
+    #"/mnt/disk1" = {
+    #  # newer drive, no errors: /dev/disk/by-id/usb-WD_Elements_2621_57584A3241363146314A4858-0:0-part1
+    #  device = "/dev/disk/by-id/usb-WD_Elements_2621_57584A3241363146314A4858-0:0-part1";
+    #  # older drive, some read errors: /dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1
+    #  fsType = "btrfs";
+    #  options = [ "defaults" "noatime" ];
+    #};
+    #"/mnt/disk2" = {
+    #  # older drive, some read errors: /dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1
+    #  device = "/dev/disk/by-id/usb-WD_Elements_1078_575831314141343244544855-0:0-part1";
+    #  fsType = "btrfs";
+    #  options = [ "defaults" "noatime" ];
+    #};
+    #"/mnt/storage" = {
+    #  device = "/mnt/disk*";
+    #  fsType = "fuse.mergerfs";
+    #  options = [
+    #    "defaults"
+    #    # partial cache required for mmap support for qbittorrent
+    #    # ref: https://github.com/trapexit/mergerfs#you-need-mmap-used-by-rtorrent-and-many-sqlite3-base-software
+    #    "cache.files=partial"
+    #    "dropcacheonclose=true"
+    #    "category.create=mfs"
+    #    "fsname=mergerfs"
+    #    # TODO enable once I actually have free space
+    #    #"minfreespace=200G"
+    #  ];
+    #};
+    #"/mnt/parity1" = {
+    #  device = "/dev/disk/by-id/usb-WDC_WD40_EFPX-68C6CN0_152D00539000-0:0-part1";
+    #  fsType = "ext4";
+    #  options = [ "defaults" "noatime" ];
+    #};
   };
 
   swapDevices = [ {
