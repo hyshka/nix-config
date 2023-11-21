dbc46b321b901a95d94a8f4feb2d8e405d9a680c
hyshka <bryan@hyshka.com>
2023-11-21 12:37:39 -0700

    tiny1: test more hass configs

--- hosts/tiny1/services/home-assistant.nix
+++ hosts/tiny1/services/home-assistant.nix
@@ -17,7 +17,14 @@
       "esphome"
       "radio_browser"
       # extras
-      "environment_canada"
+      "environment_canada" # https://www.home-assistant.io/integrations/environment_canada/
+      # station id: AB/s0000045
+      "qbittorrent" # https://www.home-assistant.io/integrations/qbittorrent/
+      "radarr" # https://www.home-assistant.io/integrations/radarr/
+      "sonarr" # https://www.home-assistant.io/integrations/sonarr/
+      "syncthing" # https://www.home-assistant.io/integrations/syncthing/
+      "glances" # https://www.home-assistant.io/integrations/glances/
+      "feedreader" # https://www.home-assistant.io/integrations/feedreader/
     ];
     # todo writable until i know what to do
     configWritable = true;
@@ -31,13 +38,46 @@
         time_zone = "America/Edmonton";
         currency = "CAD";
         country = "CA";
+        temperature_unit = "C";
         internal_url = "http://10.0.0.240:8123";
       };
       frontend = {
         themes = "!include_dir_merge_named themes";
       };
-      http = {};
       feedreader.urls = ["https://nixos.org/blogs.xml"];
+      http = {};
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+
+      # weather
+      weather = [
+        { platform = "environment_canada"; }
+      ];
+
+      # dad jokes
+      conversation.intents = {
+        TellJoke = [
+          "Tell [me] (a joke|something funny|a dad joke)"
+        ];
+      };
+      sensor = [
+        {
+          name = "random_joke";
+          platform = "rest";
+          json_attributes = "joke";
+          resource = "https://icanhazdadjoke.com/";
+          scan_interval = "3600";
+          headers.Accept = "application/json";
+        }
+      ];
+      intent_script.TellJoke = {
+        speech.text = ''{{ state_attr("sensor.random_joke", "joke") }}'';
+        action = {
+          service = "homeassistant.update_entity";
+          entity_id = "sensor.random_joke";
+        };
+      };
     };
   };
 }
