5a0e1b08d7ca5a6aa9d605c5180fc1372850049f
hyshka <bryan@hyshka.com>
2023-11-27 11:32:19 -0700

    tiny1: fix overlays

--- hosts/tiny1/configuration.nix
+++ hosts/tiny1/configuration.nix
@@ -10,7 +10,6 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
-    outputs.overlays.unstable-packages
 
     # If you want to use modules from other flakes (such as nixos-hardware):
     inputs.hardware.nixosModules.common-pc-ssd
@@ -25,6 +24,26 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      #outputs.overlays.additions
+      #outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+  };
 
   networking = {
     hostName = "tiny1";
