23a214672a14bd45487a888e6ec05e5ef8ff686b
Bryan Hyshka <bryan@hyshka.com>
2024-09-17 18:47:41 -0600

    tiny1: grafana node exporter

--- hosts/tiny1/services/grafana.nix
+++ hosts/tiny1/services/grafana.nix
@@ -1,4 +1,6 @@
-{config, ...}: {
+{config, ...}: let
+  scrapeInterval = "15s";
+in {
   sops.secrets = {
     grafana-adminPass = {
       owner = "grafana";
@@ -6,9 +8,12 @@
     };
   };
 
+  # https://wiki.nixos.org/wiki/Grafana
+  # https://github.com/nix-community/nur-combined/blob/3652fac2e8a82268eb4dcf6099144669ae253e22/repos/alarsyo/services/monitoring.nix#L40
   services.grafana = {
     enable = true;
     dataDir = "/mnt/storage/grafana";
+
     settings = {
       analytics.reporting_enabled = false;
       security.admin_password = "$__file{${config.sops.secrets.grafana-adminPass.path}}";
@@ -17,6 +22,64 @@
         http_port = 3002;
         domain = "grafana.home.hyshka.com";
         serve_from_sub_path = true;
+      };
+    };
+
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+          jsonData = {
+            timeInterval = scrapeInterval;
+          };
+        }
+      ];
+      # TODO:
+      # - https://grafana.com/grafana/dashboards/1860-node-exporter-full/
+      #dashboards.settings.providers = [
+      #  {
+      #    name = "Node Exporter";
+      #    options.path = pkgs.packages.grafanaDashboards.node-exporter;
+      #    disableDeletion = true;
+      #  }
+      #  {
+      #    name = "NGINX";
+      #    options.path = pkgs.packages.grafanaDashboards.nginx;
+      #    disableDeletion = true;
+      #  }
+      #];
+    };
+  };
+
+  # https://wiki.nixos.org/wiki/Prometheus
+  # https://github.com/Misterio77/nix-config/blob/692aca80fad823de5d39333a8ff10c271c1388f2/hosts/alcyone/services/prometheus.nix#L10
+  services.prometheus = {
+    enable = true;
+    listenAddress = "127.0.0.1";
+
+    globalConfig = {
+      scrape_interval = scrapeInterval;
+    };
+
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];
+          }
+        ];
+      }
+    ];
+
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = ["systemd"];
+        listenAddress = "127.0.0.1";
       };
     };
   };
