db90f3f7910b49debd11552ff5e4bf8573425a01
hyshka <bryan@hyshka.com>
2024-09-29 11:06:01 -0600

    Fix adguard port

--- hosts/tiny1/services/adguard-home.nix
+++ hosts/tiny1/services/adguard-home.nix
@@ -2,7 +2,7 @@
   services.adguardhome = {
     enable = true;
     #host = "127.0.0.1"; # TODO: limit to localhost
-    port = "3020";
+    port = 3020;
     # https://github.com/AdguardTeam/AdGuardHome/wiki/Configuration#configuration-file
     # settings = {}; # TODO: configure settings through module
   };
---
+++ hosts/tiny1/services/paperless.nix
@@ -0,0 +1,2 @@
+# TODO: https://search.nixos.org/options?channel=24.05&from=0&size=50&sort=relevance&type=packages&query=paperless
+{}
