ec8a8111626cd6b09770bdf08c163da3be0cdf15
hyshka <bryan@hyshka.com>
2024-09-30 19:27:57 -0600

    immich: add comments

--- hosts/tiny1/services/immich.nix
+++ hosts/tiny1/services/immich.nix
@@ -23,12 +23,14 @@
   };
 
   # TODO: database backups
+  # https://immich.app/docs/administration/backup-and-restore
   services.immich = {
     enable = true;
     mediaLocation = "/mnt/storage/immich/";
     secretsFile = config.sops.secrets.immich-secretsFile.path;
     port = 3005;
     host = "127.0.0.1";
+    # https://immich.app/docs/install/environment-variables
     environment = {
       IMMICH_TRUSTED_PROXIES = "127.0.0.1";
       IMMICH_METRICS = "true";
