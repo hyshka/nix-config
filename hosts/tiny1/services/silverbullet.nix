67c74f3762d297a8f4c665e7b962ea977e71b359
hyshka <bryan@hyshka.com>
2024-09-30 20:28:02 -0600

    tiny1: upgrade silverbullet

--- hosts/tiny1/services/silverbullet.nix
+++ hosts/tiny1/services/silverbullet.nix
@@ -1,8 +1,9 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   sops.secrets.silverbullet-envFile = {};
 
   services.silverbullet = {
     enable = true;
+    package = pkgs.unstable.silverbullet;
     spaceDir = "/mnt/storage/silverbullet";
     listenPort = 3010;
     listenAddress = "127.0.0.1";
