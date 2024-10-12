483b90ba5692c3cdcd5814ceb5c37d7c4c58ddbc
hyshka <bryan@hyshka.com>
2024-10-12 14:29:30 -0600

    tiny1: nextcloud upgrade
    - add redis cache
    - remove memories and recognize

--- hosts/tiny1/services/nextcloud.nix
+++ hosts/tiny1/services/nextcloud.nix
@@ -22,11 +22,12 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud30;
     home = "/mnt/storage/nextcloud";
     https = true;
     autoUpdateApps.enable = true;
     hostName = "localhost";
+    configureRedis = true;
 
     # Fix Nextcloud warning
     phpOptions."opcache.interned_strings_buffer" = "16";
@@ -37,15 +38,7 @@
       default_phone_region = "CA";
       maintenance_window_start = "4";
       preview_ffmpeg_path = lib.getExe pkgs.ffmpeg;
-      "memories.exiftool" = lib.getExe pkgs.exiftool;
-      "memories.ffmpeg_path" = lib.getExe' pkgs.ffmpeg "ffmpeg";
-      "memories.ffprobe_path" = lib.getExe' pkgs.ffmpeg "ffprobe";
-      "memories.vod.ffmpeg" = lib.getExe' pkgs.ffmpeg "ffmpeg";
-      "memories.vod.ffprobe" = lib.getExe' pkgs.ffmpeg "ffprobe";
       # TODO vaapi https://github.com/NuschtOS/nixos-modules/blob/fb562371c3773fc1554cf450c93db35c377f4b3b/modules/nextcloud.nix#L15
-      recognize = {
-        nice_binary = lib.getExe' pkgs.coreutils "nice";
-      };
       enabledPreviewProviders = [
         "OC\\Preview\\AVI"
         "OC\\Preview\\BMP"
@@ -75,17 +68,7 @@
     # - https://github.com/dverdonschot/nixos-systems-configuration/blob/1f7e7c7861d0243b8145baa2535a02a04efd83e5/nix-containers/nextcloud-container.nix#L84
     # - https://github.com/nmasur/dotfiles/blob/4883532c65383c2615047bd1bb3ed5cf606f996e/modules/nixos/services/nextcloud.nix#L33
 
-    # Nextcloud Memories (+Preview Generator, +Recognize)
-    # TODO Add a (system) cron job for ./occ preview:pre-generate
-    # Reverse Geocoding not supported with SQLite
-
     # Nextcloud Office
     # TODO https://wiki.nixos.org/wiki/Nextcloud#Plugins
-  };
-
-  # For Recognize
-  users.users.nextcloud = {
-    # binaries located in /etc/profiles/per-user/nextcloud/bin/
-    packages = [pkgs.nodejs pkgs.libtensorflow];
   };
 }
