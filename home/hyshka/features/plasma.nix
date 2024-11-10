db67ae2b1d337b18b3a5d34ea6c2a991219b5d89
hyshka <bryan@hyshka.com>
2024-11-09 20:29:43 -0700

    add base plasma config

--- home/hyshka/features/plasma.nix
+++ home/hyshka/features/plasma.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   pkgs,
+  config,
   ...
 }: {
   imports = [
@@ -17,4 +18,90 @@
   # TODO krohnkite key binds
   # https://github.com/phrmendes/dotfiles/blob/0deb7f64e88c165e2a3db8bdf7491dc45209c8d6/modules/plasma.nix#L4
   # https://github.com/f-koehler/nix-configs/blob/3f346f598c5dab55cdb6fc42a067c705fb91ec9b/home/modules/plasma.nix#L8
+
+  # https://github.com/phrmendes/dotfiles/blob/0deb7f64e88c165e2a3db8bdf7491dc45209c8d6/modules/plasma.nix#L10
+  # https://github.com/taj-ny/nix-config/blob/654491b314526e4d319aae5d2daaea09c8159bf2/home/config/_shared/programs/plasma/shortcuts.nix#L39
+
+  # krohnkite config
+  # https://github.com/taj-ny/nix-config/blob/654491b314526e4d319aae5d2daaea09c8159bf2/home/config/_shared/programs/plasma/kwin/krohnkite.nix
+  # https://github.com/phrmendes/dotfiles/blob/0deb7f64e88c165e2a3db8bdf7491dc45209c8d6/modules/plasma.nix#L119
+
+  programs.plasma = {
+    enable = true;
+    workspace = {
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images/5120x2880.png";
+    };
+    kscreenlocker.appearance = {
+      wallpaper = config.programs.plasma.workspace.wallpaper;
+    };
+    input = {
+    };
+    kscreenlocker = {
+      lockOnResume = true;
+      timeout = 5;
+    };
+    kwin = {
+      virtualDesktops.number = 4;
+      effects = {
+        minimization.animation = "off";
+        desktopSwitching.animation = "off";
+      };
+    };
+    shortcuts = {
+      kwin = {
+        "Kill Window" = "Alt+Shift+Q";
+        # Desktops
+        "Switch to Desktop 1" = "Alt+1";
+        "Switch to Desktop 2" = "Alt+2";
+        "Switch to Desktop 3" = "Alt+3";
+        "Switch to Desktop 4" = "Alt+4";
+        "Window to Desktop 1" = "Alt+Shift+1";
+        "Window to Desktop 2" = "Alt+Shift+2";
+        "Window to Desktop 3" = "Alt+Shift+3";
+        "Window to Desktop 4" = "Alt+Shift+4";
+        "Show Desktop" = "none";
+        # Krohnkite
+        "KrohnkiteColumnsLayout" = "Alt+Shift+C";
+        "KrohnkiteFocusDown" = "Alt+J";
+        "KrohnkiteFocusLeft" = "Alt+H";
+        "KrohnkiteFocusRight" = "Alt+L";
+        "KrohnkiteFocusUp" = "Alt+K";
+        "KrohnkiteIncrease" = "Alt+Shift+I";
+        "KrohnkiteMonocleLayout" = "Alt+Shift+M";
+        "KrohnkiteShiftDown" = "Alt+Shift+J";
+        "KrohnkiteShiftLeft" = "Alt+Shift+H";
+        "KrohnkiteShiftRight" = "Alt+Shift+L";
+        "KrohnkiteShiftUp" = "Alt+Shift+K";
+        "KrohnkiteStackedLayout" = "Alt+Shift+S";
+        "KrohnkiteToggleFloat" = "Alt+Shift+Space";
+      };
+      # Misc
+      "services/org.kde.spectacle.desktop"."RectangularRegionScreenShot" = ["Ctrl+Alt+P" "Print"];
+    };
+    spectacle.shortcuts.launch = "Print";
+    hotkeys.commands."launch-alacritty" = {
+      name = "Launch alacritty";
+      key = "Alt+Return";
+      command = "alacritty";
+    };
+    hotkeys.commands."launch-krunner" = {
+      name = "Launch krunner";
+      key = "Alt+Space";
+      command = "krunner";
+    };
+    configFile = {
+      "kcminputrc"."Libinput/1133/16500/Logitech G305"."PointerAcceleration" = "-0.800";
+      "kcminputrc"."Libinput/1267/276/Elan Touchpad"."ClickMethod" = 2;
+      "kcminputrc"."Libinput/1267/276/Elan Touchpad"."ScrollFactor" = 0.3;
+      "kdeglobals"."General"."TerminalApplication" = "alacritty";
+      "kdeglobals"."General"."TerminalService" = "Alacritty.desktop";
+      "kwinrc"."Plugins"."krohnkiteEnabled" = true;
+      "kwinrc"."Xwayland"."Scale" = 1.25;
+      "kxkbrc"."Layout"."Model" = "pc105";
+      "plasma-localerc"."Formats"."LANG" = "en_US.UTF-8";
+      "plasmanotifyrc"."Notifications"."PopupTimeout" = 3000;
+      "spectaclerc"."GuiConfig"."captureDelay" = 3;
+      "spectaclerc"."GuiConfig"."captureMode" = 0;
+    };
+  };
 }
