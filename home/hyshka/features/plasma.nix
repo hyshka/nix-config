9edcff2f204fff7b7bce9d31220e0bec5663148e
hyshka <bryan@hyshka.com>
2024-11-11 16:05:54 -0700

    plasma: add desktop 5

--- home/hyshka/features/plasma.nix
+++ home/hyshka/features/plasma.nix
@@ -93,10 +93,12 @@
         "Switch to Desktop 2" = "Alt+2";
         "Switch to Desktop 3" = "Alt+3";
         "Switch to Desktop 4" = "Alt+4";
+        "Switch to Desktop 5" = "Alt+5";
         "Window to Desktop 1" = "Alt+Shift+1";
         "Window to Desktop 2" = "Alt+Shift+2";
         "Window to Desktop 3" = "Alt+Shift+3";
         "Window to Desktop 4" = "Alt+Shift+4";
+        "Window to Desktop 5" = "Alt+Shift+5";
         "Show Desktop" = "none";
         # Krohnkite
         "KrohnkiteColumnsLayout" = "Alt+Shift+C";
