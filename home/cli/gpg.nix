1fb4566a1578bd79c8c86e8dd6f28278b28313fa
hyshka <bryan@hyshka.com>
2024-11-11 19:31:41 -0700

    Clean up ssh and gpg configs

--- home/cli/gpg.nix
+++ home/cli/gpg.nix
@@ -5,26 +5,18 @@
   ...
 }: let
   isLinux = pkgs.stdenv.isLinux;
-  #pinentry =
-  #  if config.gtk.enable then {
-  #    packages = [ pkgs.pinentry-gnome pkgs.gcr ];
-  #    name = "gnome3";
-  #  } else {
-  #    packages = [ pkgs.pinentry-curses ];
-  #    name = "curses";
-  #  };
-  pinentry = {
-    packages = [pkgs.pinentry-curses];
-    name = "curses";
-  };
+  pinentry =
+    if config.programs.plasma.enable
+    then {
+      package = pkgs.pinentry-qt;
+    }
+    else {
+      package = pkgs.pinentry-curses;
+    };
 in {
-  # TODO remove?
-  home.packages = pinentry.packages;
-
   services.gpg-agent = lib.mkIf isLinux {
     enable = true;
-    enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-curses;
+    pinentryPackage = pinentry.package;
     defaultCacheTtl = 1800; # 30 min
     defaultCacheTtlSsh = 1800; # 30 min
   };
--- home/cli/ssh.nix
+++ home/cli/ssh.nix
@@ -1,19 +1,10 @@
 {
-  outputs,
-  lib,
-  pkgs,
-  ...
-}: let
-  hostnames = builtins.attrNames outputs.nixosConfigurations;
-in {
   programs.ssh = {
     enable = true;
     serverAliveInterval = 300;
     compression = true;
     controlMaster = "auto";
     controlPersist = "3s";
-    # TODO mkdir -p ~/.ssh/master-socket required before this will work
-    controlPath = "~/.ssh/master-socket/%r@%h:%p";
     matchBlocks = {
       # Force gpg-agent to open in current terminal
       # Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
@@ -22,58 +13,26 @@ in {
         match = ''host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"'';
       };
 
-      #  net = {
-      #    host = builtins.concatStringsSep " " hostnames;
-      #    forwardAgent = true;
-      #    remoteForwards = [{
-      #      bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
-      #      host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
-      #    }];
-      #  };
+      # These are only accessible within the tailnet
       tiny1 = {
         host = "tiny1";
-        hostname = "10.0.0.240";
-        port = 38000;
+        hostname = "tiny1";
         user = "hyshka";
       };
-      tiny1Ex = {
-        host = "tiny1Ex";
-        hostname = "68.148.153.144";
-        port = 38000;
-        user = "hyshka";
-      };
-      #rpi4 = {
-      #  host = "rpi4";
-      #  hostname = "10.0.0.250";
-      #  port = 38002;
-      #  user = "hyshka";
-      #};
       rpi4 = {
         host = "rpi4";
-        hostname = "10.0.0.241";
-        port = 38002;
+        hostname = "rpi4";
         user = "hyshka";
-      };
-      rpi4Ex = {
-        host = "rpi4Ex";
-        hostname = "10.0.0.241";
-        port = 38002;
-        user = "hyshka";
-        proxyJump = "hyshka@68.148.153.144:38000";
       };
       starship = {
         host = "starship";
-        hostname = "10.0.0.251";
-        port = 38001;
+        hostname = "starship";
         user = "hyshka";
-        proxyJump = "hyshka@10.0.0.250:38000";
       };
-      starshipEx = {
-        host = "starshipEx";
-        hostname = "10.0.0.251";
-        port = 38001;
+      ashyn = {
+        host = "ashyn";
+        hostname = "ashyn";
         user = "hyshka";
-        proxyJump = "hyshka@68.148.153.144:38000";
       };
     };
   };
