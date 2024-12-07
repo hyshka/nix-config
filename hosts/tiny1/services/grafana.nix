94fd1c9e4f9d4fee784403123b7b3bfe176af8fc
hyshka <bryan@hyshka.com>
2024-12-07 14:50:13 -0700

    Grafana: clean up

--- hosts/tiny1/services/grafana.nix
+++ hosts/tiny1/services/grafana.nix
@@ -1,6 +1,4 @@
-{config, ...}: let
-  scrapeInterval = "15s";
-in {
+{config, ...}: {
   sops.secrets = {
     grafana-adminPass = {
       owner = "grafana";
@@ -22,28 +20,64 @@ in {
         http_addr = "127.0.0.1";
         http_port = 3002;
         domain = "grafana.home.hyshka.com";
+        root_url = "https://grafana.home.hyshka.com";
         serve_from_sub_path = true;
       };
     };
 
     provision = {
       enable = true;
-      datasources.settings.datasources = [
-        {
-          name = "Prometheus";
-          type = "prometheus";
-          url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
-          jsonData = {
-            timeInterval = scrapeInterval;
-          };
-          isDefault = true;
-        }
-        {
-          name = "Loki";
-          type = "loki";
-          url = "http://localhost:${builtins.toString config.services.loki.configuration.server.http_listen_port}";
-        }
-      ];
+      datasources.settings = {
+        datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+            isDefault = true;
+          }
+          {
+            name = "Loki";
+            type = "loki";
+            url = "http://localhost:${builtins.toString config.services.loki.configuration.server.http_listen_port}";
+          }
+        ];
+        deleteDatasources = [
+          {
+            name = "Prometheus";
+          }
+          {
+            name = "Loki";
+          }
+        ];
+      };
+      # TODO
+      # https://github.com/ibizaman/selfhostblocks/blob/main/modules/blocks/monitoring.nix#L241
+      #alerting.contactPoints.settings = {
+      #  contactPoints = [{
+      #    name = "ntfy";
+      #    receivers = [{
+      #      uid = "sysadmin";
+      #      type = "webhook";
+      #      settings = {
+      #        url = "http://localhost:2586/grafana";
+      #        http_method = "POST";
+      #        authorization_header = secret_token;
+      #      };
+      #    }];
+      #  }];
+      #};
+      alerting.policies.settings = {
+        policies = [
+          {
+            receiver = "webhook";
+            group_by = ["grafana_folder" "alertname"];
+            group_wait = "30s";
+            group_interval = "5m";
+            repeat_interval = "4h";
+          }
+        ];
+      };
+
       # TODO:
       # - https://grafana.com/grafana/dashboards/1860-node-exporter-full/
       # - https://grafana.com/grafana/dashboards/20802-caddy-monitoring/
@@ -69,10 +103,6 @@ in {
   services.prometheus = {
     enable = true;
     listenAddress = "127.0.0.1";
-
-    globalConfig = {
-      scrape_interval = scrapeInterval;
-    };
 
     scrapeConfigs = [
       {
