a0d28b0b9c061093c818b2bd0fd51f018af04bdb
hyshka <bryan@hyshka.com>
2024-12-27 15:20:24 -0700

    tiny1: add memtest86

--- hosts/tiny1/default.nix
+++ hosts/tiny1/default.nix
@@ -22,6 +22,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
 
   environment.systemPackages = with pkgs; [
     # utils
