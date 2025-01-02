df96749536a7d96ee7672380556a2e5552395d3c
hyshka <bryan@hyshka.com>
2025-01-02 11:19:03 -0700

    nixvim: default model, quick chat

--- home/nixvim/plugins/custom/copilot.nix
+++ home/nixvim/plugins/custom/copilot.nix
@@ -19,26 +19,12 @@
             insert = "";
           };
         };
+        model = "claude-3.5-sonnet";
       };
     };
 
     # Ref: https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#tips
     keymaps = [
-      {
-        mode = "n";
-        key = "<leader>cch";
-        action = {
-          __raw = ''
-            function()
-              local actions = require("CopilotChat.actions")
-              require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-            end
-          '';
-        };
-        options = {
-          desc = "[C]opilot[C]hat: [H]elp actions";
-        };
-      }
       {
         mode = "n";
         key = "<leader>ccp";
@@ -51,6 +37,22 @@
         };
         options = {
           desc = "[C]opilot[C]hat: [P]rompt actions";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>ccq";
+        action = {
+          __raw = ''
+            function()
+              local input = vim.fn.input("Quick Chat: ")
+              if input ~= "" then
+                require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+              end
+            end'';
+        };
+        options = {
+          desc = "[C]opilot[C]hat: [Q]uick chat";
         };
       }
     ];
