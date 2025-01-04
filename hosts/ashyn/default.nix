00917c7b16dfbec415cbcd931dc06a1627ac6ce3
hyshka <bryan@hyshka.com>
2025-01-04 16:20:51 -0700

    bump all inputs

--- hosts/ashyn/default.nix
+++ hosts/ashyn/default.nix
@@ -14,6 +14,10 @@
     ../common/optional/wireless.nix
   ];
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "deskflow-1.18.0"
+  ];
+
   networking.hostName = "ashyn";
 
   # https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
