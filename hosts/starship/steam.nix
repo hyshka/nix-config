7f973e845a67041f87512a619f710962e05927f9
hyshka <bryan@hyshka.com>
2025-02-09 07:18:03 -0700

    Add steam gamescope config

--- hosts/starship/steam.nix
+++ hosts/starship/steam.nix
@@ -2,6 +2,18 @@
   programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
+    gamescopeSession = {
+      enable = true;
+      # Phone res 1080 x 2280 pixels, 19:9 ratio
+      # TODO: read SUNSHINE_CLIENT_WIDTH SUNSHINE_CLIENT_HEIGHT SUNSHINE_CLIENT_FPS
+      args = [
+        "-w 2280"
+        "-h 1080"
+        "-r 60"
+        "-o 60"
+        "-S integer"
+      ];
+    };
+    protontricks.enable = true;
   };
-  hardware.steam-hardware.enable = true;
 }
--- hosts/starship/sunshine.nix
+++ hosts/starship/sunshine.nix
@@ -3,7 +3,30 @@
     enable = true;
     openFirewall = true;
     capSysAdmin = true;
+    # TODO
+    #applications = {
+    #  env = {
+    #    PATH = "$(PATH):$(HOME)/.local/bin";
+    #  };
+    #  apps = [
+    #    {
+    #      name = "1440p Desktop";
+    #      prep-cmd = [
+    #        {
+    #          do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.2560x1440@144";
+    #          undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.3440x1440@144";
+    #        }
+    #      ];
+    #      exclude-global-prep-cmd = "false";
+    #      auto-detach = "true";
+    #    }
+    #  ];
+    #};
   };
+  # Start sunshine app
+  # kscreen-doctor output.DP-1.mode.${SUNSHINE_CLIENT_WIDTH}x${SUNSHINE_CLIENT_HEIGHT}@${SUNSHINE_CLIENT_FPS}
+  # Stop app
+  # kscreen-doctor output.DP-1.mode.2560x1440@240
 
   services.avahi = {
     enable = true;
