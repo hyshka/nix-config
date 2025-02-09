9d2b1143e39022ab2396a7c162e1b84e784e52b6
hyshka <bryan@hyshka.com>
2025-02-09 08:04:47 -0700

    refactor: use nixpkgs in sunshine apps

--- hosts/starship/sunshine.nix
+++ hosts/starship/sunshine.nix
@@ -1,42 +1,29 @@
-{...}: {
+{pkgs, ...}: {
   services.sunshine = {
     enable = true;
     openFirewall = true;
     capSysAdmin = true;
-    # TODO
-    #applications = {
-    #  env = {
-    #    PATH = "$(PATH):$(HOME)/.local/bin";
-    #  };
-    #  apps = [
-    #    {
-    #      name = "1440p Desktop";
-    #      prep-cmd = [
-    #        {
-    #          do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.2560x1440@144";
-    #          undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.3440x1440@144";
-    #        }
-    #      ];
-    #      exclude-global-prep-cmd = "false";
-    #      auto-detach = "true";
-    #    }
-    #  ];
-    #};
-  };
-  # Start sunshine app
-  # kscreen-doctor output.DP-1.mode.${SUNSHINE_CLIENT_WIDTH}x${SUNSHINE_CLIENT_HEIGHT}@${SUNSHINE_CLIENT_FPS}
-  # Stop app
-  # kscreen-doctor output.DP-1.mode.2560x1440@240
-
-  services.avahi = {
-    enable = true;
-    reflector = true;
-    nssmdns4 = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      userServices = true;
-      workstation = true;
+    applications = {
+      apps = [
+        {
+          name = "Desktop";
+          image-path = "desktop.png";
+        }
+        {
+          name = "Steam Big Picture";
+          image-path = "steam.png";
+          detatched = [
+            "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"
+          ];
+        }
+        {
+          name = "Steam Gamescope Big Picture";
+          image-path = "steam.png";
+          detatched = [
+            "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam-gamescope steam://open/bigpicture"
+          ];
+        }
+      ];
     };
   };
 }
