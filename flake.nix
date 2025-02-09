b476a46b194236df3c3fef372a96b0bde3328ee7
hyshka <bryan@hyshka.com>
2025-02-08 18:26:22 -0700

    Remove stable input

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
