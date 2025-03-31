ca574cfc660ee3273d47dbd9700260b3d9a8770f
hyshka <bryan@hyshka.com>
2025-03-31 10:59:07 -0600

    flake: fix build warnings

--- home/hyshka/global/default.nix
+++ home/hyshka/global/default.nix
@@ -10,7 +10,7 @@
 in {
   imports =
     [
-      inputs.catppuccin.homeManagerModules.catppuccin
+      inputs.catppuccin.homeModules.catppuccin
       #../features/cli
       #../features/nvim
     ]
