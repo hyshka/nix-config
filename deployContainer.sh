99a21a46e359aa09b209bb4695b98cd340eb98f9
hyshka <bryan@hyshka.com>
2025-04-19 14:59:24 -0600

    incus: extract build_id to make path more robust

--- deployContainer.sh
+++ deployContainer.sh
@@ -7,12 +7,14 @@ if [ -z "$containers" ]; then
     exit 2
 fi
 
+NIXOS_BUILD_ID=$(awk -F= '/^BUILD_ID=/ {gsub(/"/, "", $2); print $2}' /etc/os-release)
+
 # TODO: I only have one remote, but I could add the host as a param to the script
 incus remote switch tiny1
 
 for container in ${containers//,/ }; do
     incus image import --alias nixos/custom/$container --reuse \
-    $(nix build ".#nixosConfigurations.$container.config.system.build.metadata" --print-out-paths)/tarball/nixos-image-lxc-25.05.20250102.6df2492-x86_64-linux.tar.xz \
+    $(nix build ".#nixosConfigurations.$container.config.system.build.metadata" --print-out-paths)/tarball/nixos-image-lxc-${NIXOS_BUILD_ID}-x86_64-linux.tar.xz \
     $(nix build ".#nixosConfigurations.$container.config.system.build.squashfs" --print-out-paths)/nixos-lxc-image-x86_64-linux.squashfs
 
     incus rebuild nixos/custom/$container $container --force
