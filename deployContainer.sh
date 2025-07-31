13331e30d21ae73dd7ddf020026011de71ecf337
hyshka <bryan@hyshka.com>
2025-07-31 16:24:55 -0600

    deployContainer: use eval BUILD_ID instead of active derivation from system

--- deployContainer.sh
+++ deployContainer.sh
@@ -7,12 +7,11 @@ if [ -z "$containers" ]; then
     exit 2
 fi
 
-NIXOS_BUILD_ID=$(awk -F= '/^BUILD_ID=/ {gsub(/"/, "", $2); print $2}' /etc/os-release)
-
 # TODO: I only have one remote, but I could add the host as a param to the script
 incus remote switch tiny1
 
 for container in ${containers//,/ }; do
+    NIXOS_BUILD_ID=$(nix eval --raw ".#nixosConfigurations.$container.config.system.nixos.version")
     incus image import --alias nixos/custom/$container --reuse \
     $(nix build ".#nixosConfigurations.$container.config.system.build.metadata" --print-out-paths)/tarball/nixos-image-lxc-${NIXOS_BUILD_ID}-x86_64-linux.tar.xz \
     $(nix build ".#nixosConfigurations.$container.config.system.build.squashfs" --print-out-paths)/nixos-lxc-image-x86_64-linux.squashfs
