f8d15935ce573a335e09ce594294b06342b46f94
hyshka <bryan@hyshka.com>
2025-08-01 10:17:18 -0600

    nixvim: add mcphub to copilotchat

--- home/nixvim/plugins/custom/mcphub.nix
+++ home/nixvim/plugins/custom/mcphub.nix
@@ -10,7 +10,16 @@ in {
     extraPackages = [mcp-hub];
     extraPlugins = [mcphub-nvim];
     extraConfigLua = ''
-      require("mcphub").setup()
+      require("mcphub").setup({
+        extensions = {
+          copilotchat = {
+            enabled = true,
+            convert_tools_to_functions = true,
+            convert_resources_to_functions = true,
+            add_mcp_prefix = false,
+          }
+        }
+      })
     '';
   };
   # TODO: declarative MCP server configuration
