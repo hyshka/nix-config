3bf046479abf42305dbf6c658c1fa618df3bc028
hyshka <bryan@hyshka.com>
2025-08-08 20:21:12 -0600

    sunshine: update apps

--- hosts/starship/sunshine.nix
+++ hosts/starship/sunshine.nix
@@ -10,14 +10,33 @@
           image-path = "desktop.png";
         }
         {
-          name = "Steam Big Picture";
+          name = "Steam (1440p)";
           image-path = "steam.png";
           detached = [
             "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"
           ];
+          prep-cmd = [
+            {
+              undo = "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://close/bigpicture";
+            }
+          ];
         }
         {
-          name = "Steam Gamescope";
+          name = "Steam (480p)";
+          image-path = "steam.png";
+          detached = [
+            "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"
+          ];
+          prep-cmd = [
+            {
+              # TODO: ${SUNSHINE_CLIENT_WIDTH}x${SUNSHINE_CLIENT_HEIGHT}@${SUNSHINE_CLIENT_FPS}
+              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.640x480@60";
+              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.2560x1440@240 && ${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://close/bigpicture";
+            }
+          ];
+        }
+        {
+          name = "Steam Gamescope (480p)";
           image-path = "steam.png";
           detached = [
             "setsid steam-gamescope"
@@ -25,8 +44,8 @@
           prep-cmd = [
             {
               # TODO: ${SUNSHINE_CLIENT_WIDTH}x${SUNSHINE_CLIENT_HEIGHT}@${SUNSHINE_CLIENT_FPS}
-              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.1280x720@60";
-              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.2560x1440@240";
+              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.640x480@60";
+              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-1.mode.2560x1440@240 && ${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://close/bigpicture";
             }
           ];
         }
