bc71b4c3c6c2245578f1e86e2aec8bfb20cbf2a1
hyshka <bryan@hyshka.com>
2025-09-06 18:34:26 -0600

    services todos

--- hosts/tiny1/services/default.nix
+++ hosts/tiny1/services/default.nix
@@ -9,9 +9,9 @@
     ./samba.nix
     ./snapraid.nix
     ./syncthing.nix # TODO: move to incus
-    ./caddy.nix
+    ./caddy.nix # TODO: move to incus
     ./acme.nix
-    #./nextcloud.nix
+    #./nextcloud.nix # TODO: move to incus
     ./grafana # TODO: move to incus
     ./adguard-home.nix # TODO: move to rpi4
     ./incus.nix
