c3750f017ed0be170734b2174a48be24ea86af25
hyshka <bryan@hyshka.com>
2025-09-26 10:32:24 -0600

    ssh: update deprecated syntax

--- home/cli/ssh.nix
+++ home/cli/ssh.nix
@@ -1,11 +1,22 @@
 {
   programs.ssh = {
     enable = true;
-    serverAliveInterval = 300;
-    compression = true;
-    controlMaster = "auto";
-    controlPersist = "3s";
+    enableDefaultConfig = false;
     matchBlocks = {
+      # Sane defaults
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+
       # Force gpg-agent to open in current terminal
       # Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
       # https://wiki.archlinux.org/title/GnuPG#Configure_pinentry_to_use_the_correct_TTY
