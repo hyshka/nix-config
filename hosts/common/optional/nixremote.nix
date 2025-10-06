65429536da2a1c185c27f2f131a8a6558a6c7eff
hyshka <bryan@hyshka.com>
2025-10-05 18:42:01 -0600

    create nixremote group

--- hosts/common/optional/nixremote.nix
+++ hosts/common/optional/nixremote.nix
@@ -1,8 +1,9 @@
 {
+  users.groups.nixremote = {};
   users.users.nixremote = {
     description = "Nix Remote Builder User";
     isNormalUser = true;
-    group = "";
+    group = "nixremote";
     # Don't allow shell access
     useDefaultShell = false;
     # Disable su and password login
@@ -14,5 +15,5 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED7CbbCMdI59FzAowgxS9xHUEd9iF2qt3iA5McxCtqF root@ashyn"
     ];
   };
-  nix.settings.trusted-users = [ "nixremote" ];
+  nix.settings.trusted-users = ["nixremote"];
 }
