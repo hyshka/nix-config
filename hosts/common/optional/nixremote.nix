63ae7b7768a480f54f563348f53eea6a3c913063
hyshka <bryan@hyshka.com>
2025-10-05 18:39:25 -0600

    remove nixremote group

--- hosts/common/optional/nixremote.nix
+++ hosts/common/optional/nixremote.nix
@@ -2,7 +2,7 @@
   users.users.nixremote = {
     description = "Nix Remote Builder User";
     isNormalUser = true;
-    group = "nixremote";
+    group = "";
     # Don't allow shell access
     useDefaultShell = false;
     # Disable su and password login
@@ -14,5 +14,5 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED7CbbCMdI59FzAowgxS9xHUEd9iF2qt3iA5McxCtqF root@ashyn"
     ];
   };
-  nix.settings.trusted-users = ["nixremote"];
+  nix.settings.trusted-users = [ "nixremote" ];
 }
