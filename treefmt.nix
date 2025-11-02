e209a1650b09048b928ddaabeba1108a5b236a35
hyshka <bryan@hyshka.com>
2025-11-02 11:25:14 -0700

    treefmt: fix system check

--- treefmt.nix
+++ treefmt.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
 {
   projectRootFile = "flake.nix";
   programs.deadnix.enable = true;
   programs.nixfmt.enable = true;
   settings.formatter.nixfmt.excludes = [ "home/nixvim/plugins/custom/codecompanion.nix" ];
-  programs.shellcheck.enable = pkgs.hostPlatform.system != "riscv64-linux";
-  programs.shfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.shellcheck.enable = true;
+  programs.shfmt.enable = true;
 }
