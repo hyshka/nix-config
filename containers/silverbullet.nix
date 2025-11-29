b8c514393aa1faa9b0625a26745ef2ded73e7d2f
hyshka <bryan@hyshka.com>
2025-11-29 16:27:13 -0700

    silverbullet: bump to 2.3.0

--- containers/silverbullet.nix
+++ containers/silverbullet.nix
@@ -17,7 +17,7 @@ container.mkContainer {
   };
 
   virtualisation.oci-containers.containers.silverbullet = {
-    image = "ghcr.io/silverbulletmd/silverbullet:2.2.1";
+    image = "ghcr.io/silverbulletmd/silverbullet:2.3.0";
     ports = [ "0.0.0.0:3000:3000" ];
     volumes = [ "/var/lib/silverbullet:/space" ];
     environmentFiles = [ config.sops.secrets.environmentFile.path ];
