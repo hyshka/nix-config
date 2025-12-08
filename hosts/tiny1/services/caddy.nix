2f95dbde3c184e76acf777e4bad490bd697ad231
hyshka <bryan@hyshka.com>
2025-12-07 19:34:28 -0700

    caddy: fix typo

--- hosts/tiny1/services/caddy.nix
+++ hosts/tiny1/services/caddy.nix
@@ -77,7 +77,7 @@
            reverse_proxy http://10.223.27.125:2283
          }
 
-         @immich-kios host immich-kiosk.home.hyshka.com
+         @immich-kiosk host immich-kiosk.home.hyshka.com
          handle @immich-kiosk {
            reverse_proxy http://10.223.27.216:3000
          }
