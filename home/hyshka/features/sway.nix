afed20755a3e77e1c0c67434b64c671b8251e79a
hyshka <bryan@hyshka.com>
2025-12-20 19:19:48 -0700

    ashyn: update sway config, barebones

--- home/hyshka/features/sway.nix
+++ home/hyshka/features/sway.nix
@@ -1,15 +1,59 @@
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   wayland.windowManager.sway = {
+    systemd.variables = [ "--all" ];
     enable = true;
     wrapperFeatures.gtk = true;
     config = {
-      modifier = "Mod4";
+      modifier = "Mod1";
       terminal = "alacritty";
       startup = [
         { command = "brave"; }
       ];
+      menu = "wofi --show run";
+      bars = [
+        {
+          command = "waybar";
+        }
+      ];
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in
+        lib.mkOptionDefault {
+          # Desktop Utilities
+          "${modifier}+c" = "exec ${pkgs.clipman}/bin/clipman pick -t wofi";
+
+          # Main app shortcuts
+          "${modifier}+Shift+w" = "exec ${pkgs.brave}/bin/brave";
+          "${modifier}+Shift+v" = "exec ${pkgs.pavucontrol}/bin/pavucontrol";
+        };
     };
   };
-  wayland.windowManager.sway.systemd.variables = [ "--all" ];
-
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        height = 30;
+        modules-left = [
+          "sway/workspaces"
+          "sway/mode"
+          "sway/scratchpad"
+        ];
+        modules-center = [ "sway/window" ];
+        modules-right = [
+          "clock"
+          "tray"
+        ];
+      };
+    };
+  };
+  programs.swaylock.enable = true;
+  programs.wofi.enable = true;
+  services.swayidle.enable = true;
 }
