deab891501b4ab405eee46713814593b5f52b733
hyshka <bryan@hyshka.com>
2025-12-26 07:40:22 -0700

    ashyn: need to avoid powersave, too slow

--- hosts/common/optional/battery.nix
+++ hosts/common/optional/battery.nix
@@ -10,7 +10,7 @@
       CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 1;
 
