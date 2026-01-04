7254e9c10ea5d6b24eff35fcc1df11f885c8adc9
hyshka <bryan@hyshka.com>
2026-01-03 19:17:01 -0700

    caddy: update sha

--- hosts/nixos/tiny1/services/caddy.nix
+++ hosts/nixos/tiny1/services/caddy.nix
@@ -21,7 +21,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-      hash = "sha256-Dvifm7rRwFfgXfcYvXcPDNlMaoxKd5h4mHEK6kJ+T4A=";
+      hash = "sha256-Zls+5kWd/JSQsmZC4SRQ/WS+pUcRolNaaI7UQoPzJA0=";
     };
     email = "bryan@hyshka.com";
     logFormat = ''
