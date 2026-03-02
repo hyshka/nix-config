97d7317718b269608f72ed67540d1f9a0a4e73cf
hyshka <bryan@hyshka.com>
2026-03-01 19:48:24 -0700

    update recyclarr config

--- containers/media-arr.nix
+++ containers/media-arr.nix
@@ -66,6 +66,21 @@ in
           api_key = config.sops.secrets.radarr_api_key.path;
           base_url = "http://localhost:7878";
           instance_name = "main";
+          delete_old_custom_formats = true;
+          replace_existing_custom_formats = true;
+          include = [
+            { template = "radarr-quality-definition-movie"; }
+            { template = "radarr-quality-profile-hd-bluray-web"; }
+            { template = "radarr-custom-formats-hd-bluray-web"; }
+          ];
+          custom_formats = [
+            {
+              assign_scores_to = [ { name = "HD Bluray + WEB"; } ];
+              trash_ids = [
+                "839bea857ed2c0a8e084f3cbdbd65ecb" # x265 (no HDR/DV)
+              ];
+            }
+          ];
         }
       ];
       sonarr = [
@@ -73,6 +88,21 @@ in
           api_key = config.sops.secrets.sonarr_api_key.path;
           base_url = "http://localhost:8989";
           instance_name = "main";
+          delete_old_custom_formats = true;
+          replace_existing_custom_formats = true;
+          include = [
+            { template = "sonarr-quality-definition-series"; }
+            { template = "sonarr-v4-quality-profile-web-1080p-alternative"; }
+            { template = "sonarr-v4-custom-formats-web-1080p"; }
+          ];
+          custom_formats = [
+            {
+              assign_scores_to = [ { name = "WEB-1080p"; } ];
+              trash_ids = [
+                "9b64dff695c2115facf1b6ea59c9bd07" # x265 (no HDR/DV)
+              ];
+            }
+          ];
         }
       ];
     };
