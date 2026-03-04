ef3cf1d9d992dcd31ff2e1513a9cde2c15531fd0
hyshka <bryan@hyshka.com>
2026-03-04 12:56:49 -0700

    ai: listen to all interfaces

--- containers/ai.nix
+++ containers/ai.nix
@@ -10,11 +10,16 @@ in
 {
   imports = [ (container.mkContainer { name = "ai"; }) ];
 
+  # Set up
+  # - incus config device add ai persist disk "source=/persist/ai" "path=/persist" "shift=true"
+  # - incus config device add ai gpu gpu pci=0000:2b:00.0
+
   environment.systemPackages = with pkgs; [ radeontop ];
 
   services.open-webui = {
     enable = true;
     openFirewall = true;
+    host = "0.0.0.0";
     environment = {
       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
     };
@@ -32,6 +37,7 @@ in
   services.ollama = {
     enable = true;
     openFirewall = true;
+    host = "0.0.0.0";
     # Optional: preload models, see https://ollama.com/library
     #loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
     package = pkgs.ollama-rocm;
