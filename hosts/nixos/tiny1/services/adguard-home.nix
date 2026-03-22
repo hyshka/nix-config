90194bec86e8567160986447f2922fba60fb2f86
hyshka <bryan@hyshka.com>
2026-03-22 14:39:47 -0600

    adguard: fix bad schema version

--- hosts/nixos/tiny1/services/adguard-home.nix
+++ hosts/nixos/tiny1/services/adguard-home.nix
@@ -18,7 +18,7 @@ in
     port = 3020;
     allowDHCP = true;
     settings = {
-      schema_version = 33;
+      schema_version = 32;
       http = {
         address = "0.0.0.0:${toString config.services.adguardhome.port}";
       };
