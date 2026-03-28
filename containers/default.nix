0cebf3ba23c9a69cfed9fa7bd575a5943b888e18
hyshka <bryan@hyshka.com>
2026-03-28 14:08:32 -0600

    consolidate container sops keys, migrate caddy

--- containers/default.nix
+++ containers/default.nix
@@ -24,20 +24,11 @@
       };
       system.stateVersion = stateVersion;
 
-      # Ensure directory exists early in boot
-      system.activationScripts.createSSHHostKeysDir = lib.mkBefore ''
-        mkdir -p /persist/etc/ssh
-      '';
-
-      # Sops needs acess to the keys before the persist dirs are even mounted; so
-      # just persisting the keys won't work, we must point both at /persist
-      # https://github.com/nix-community/impermanence/issues/282
-      services.openssh.hostKeys = [
-        {
-          path = "/persist/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
+      # incus config device add <container> sops-age-key disk \
+      # source=tmpfs:/run/secrets/lxc-core-age-key \
+      # path=/var/lib/sops-nix/key.txt \
+      # readonly=true
+      sops.age.keyFile = "/var/lib/sops-nix/key.txt";
 
       # incus config device add paperless persist disk source=/persist/microvms/paperless path=/persist shift=true
       # Device only exists at runtime, fake it for now to build the rootfs
@@ -66,6 +57,7 @@
       programs.bash.completion.enable = false;
       fonts.fontconfig.enable = false;
       appstream.enable = false;
+      services.openssh.enable = false;
 
       # overlayfs mount requires CAP_SYS_ADMIN
       # can't go fully perlless until NixOS supports systemd-tmpfiles /etc management
