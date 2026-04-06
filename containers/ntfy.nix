03ba9bc7ab01c8c629c502080b7aa91442f4217c
hyshka <bryan@hyshka.com>
2026-04-06 06:44:12 -0600

    ntfy: switch to env var auth

--- containers/ntfy.nix
+++ containers/ntfy.nix
@@ -17,30 +17,35 @@ in
     9091
   ];
 
-  sops.secrets."ntfy_user.db" = {
-    sopsFile = ./secrets/ntfy_user.db;
-    format = "binary";
+  sops.secrets."environment_file" = {
+    sopsFile = ./secrets/ntfy.yaml;
     owner = config.services.ntfy-sh.user;
     group = config.services.ntfy-sh.group;
-    mode = "0600";
   };
 
   services.ntfy-sh = {
     enable = true;
+    environmentFile = config.sops.secrets."environment_file".path;
     settings = {
       base-url = "https://ntfy.home.hyshka.com";
       behind-proxy = true;
-      # bind to all interfaces to receive connections from Docker containers
       listen-http = "0.0.0.0:2586";
       log-format = "json";
       log-file = "/var/log/ntfy/ntfy.log";
       metrics-listen-http = "0.0.0.0:9091";
-      auth-file = config.sops.secrets."ntfy_user.db".path;
+      cache-file = "/var/lib/ntfy-sh/cache-file.db"; # persist messages across reboots
       auth-default-access = "deny-all";
+      # Auth user-file, auth-users, auth-tokens condifured in environment_file
     };
   };
 
   systemd.services.ntfy-sh.serviceConfig = {
     LogsDirectory = [ "ntfy" ];
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      "/var/lib/private/ntfy-sh"
+    ];
   };
 }
