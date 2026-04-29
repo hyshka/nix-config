23a0b07ec06b944c086a9b2ba7eb0b12ec57caa7
hyshka <bryan@hyshka.com>
2026-04-29 12:51:37 -0600

    starship: enable overclocking

--- hosts/nixos/starship/hardware-configuration.nix
+++ hosts/nixos/starship/hardware-configuration.nix
@@ -72,6 +72,8 @@
   ];
   zramSwap.enable = true;
 
+  hardware.amdgpu.overdrive.enable = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
