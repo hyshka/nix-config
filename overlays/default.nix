8c380e77e298935f36a9b19acc0fbec73cb0bd66
hyshka <bryan@hyshka.com>
2026-09-21 08:19:45 -0600

    bump flake.lock, clean up overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = _final: prev: {
+  modifications = final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
@@ -18,51 +18,25 @@
     # Pin Incus to version 6.17.0
     incus = inputs.nixpkgs-incus-6-18.legacyPackages.${prev.stdenv.hostPlatform.system}.incus;
 
-    sunshine = prev.sunshine.overrideAttrs (_oldAttrs: rec {
+    sunshine = prev.sunshine.overrideAttrs (_oldAttrs: {
       extraLibraries = [
         prev.util-linux # required for setsid
       ];
     });
 
-    # https://github.com/NixOS/nixpkgs/pull/476394
-    pocket-id = prev.pocket-id.overrideAttrs (_oldAttrs: rec {
-      version = "2.0.0";
-      src = prev.fetchFromGitHub {
-        owner = "pocket-id";
-        repo = "pocket-id";
-        tag = "v${version}";
-        hash = "sha256-jvC2m+sksVSn1pMH0tSM+r5W1VZUEHQJKzQT8GPgspI=";
-      };
-      vendorHash = "sha256-hMhOG/2xnI/adjg8CnA0tRBD8/OFDsTloFXC8iwxlV0=";
-
-      frontend = prev.stdenvNoCC.mkDerivation {
-        pname = "pocket-id-frontend";
-        inherit version src;
-        nativeBuildInputs = [
-          prev.nodejs
-          prev.pnpmConfigHook
-          prev.pnpm_10
-        ];
-        pnpmDeps = prev.fetchPnpmDeps {
-          pname = "pocket-id";
-          inherit version src;
-          pnpm = prev.pnpm_10;
-          hash = "sha256-dIhNxUBt+jxUp5I4cPZ6/PtNSyMTd6xkX6XE2SwD+ok=";
-        };
-        env.BUILD_OUTPUT_PATH = "dist";
-        buildPhase = ''
-          runHook preBuild
-          pnpm --filter pocket-id-frontend build
-          runHook postBuild
-        '';
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/lib/pocket-id-frontend
-          cp -r frontend/dist $out/lib/pocket-id-frontend/dist
-          runHook postInstall
-        '';
-      };
-    });
+    # Temporary fix: https://github.com/NixOS/nixpkgs/pull/564140/
+    tree-sitter-grammars = prev.tree-sitter-grammars.overrideScope (
+      _grammarFinal: grammarPrev: {
+        tree-sitter-cuda = grammarPrev.tree-sitter-cuda.overrideAttrs (_oldAttrs: {
+          src = final.fetchFromGitHub {
+            owner = "tree-sitter-grammars";
+            repo = "tree-sitter-cuda";
+            rev = "v0.21.2";
+            hash = "sha256-s2qrZx5fEu/I6xE2paX/Nlmgvo6T27qqvy1cI8iznAA=";
+          };
+        });
+      }
+    );
   };
 
   # Adds pkgs.stable == inputs.nixpkgs-stable.legacyPackages.${pkgs.system}
