c498ea3d1b5600c23c2b39bafe8e1174fb9c2041
hyshka <bryan@hyshka.com>
2026-09-20 18:46:55 -0600

    bump silverbullet

--- containers/silverbullet.nix
+++ containers/silverbullet.nix
@@ -16,7 +16,7 @@ in
   };
 
   virtualisation.oci-containers.containers.silverbullet = {
-    image = "ghcr.io/silverbulletmd/silverbullet:2.4.1";
+    image = "ghcr.io/silverbulletmd/silverbullet:2.11.0";
     ports = [ "0.0.0.0:3000:3000" ];
     volumes = [ "/var/lib/silverbullet:/space" ];
     environmentFiles = [ config.sops.secrets.environmentFile.path ];
